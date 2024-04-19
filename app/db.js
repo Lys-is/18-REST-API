8d6ffd83db86e14ce5da8887da933bf449287e5c
Lys-is <nikneup@yandex.ru>
2024-04-19 18:58:53 +0300

    fds

--- app/db.js
+++ app/db.js
@@ -1,12 +1,10 @@
 var sqlite3 = require('sqlite3').verbose()
-var bcrypt = require('bcrypt')
 
 const DBSOURCE = "db.sqlite" 
 
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
-      // Cannot open database
       console.error(err.message)
       throw err
     }else{
@@ -22,7 +20,7 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
         if (err) {
             console.log(err)
         }else{
-           console.log("Таблица в параше")
+           console.log("all good")
         }
     })  
     }
